1ea9a5462a365124f5988c8f3f27efe45eec47e4
Der Bejijing <62348565+DerBejijing@users.noreply.github.com>
2023-01-14 19:11:58 +0100

    Add files via upload

--- include/seq_seven_segment.h
+++ include/seq_seven_segment.h
@@ -2,16 +2,20 @@
 #define SEQ_SEVEN_SEGMENT_H
 
 #include <pico/stdlib.h>
+#include <math.h>
+
+#include "hardware/gpio.h"
 
 #include "seq_globals.h"
 
-void seven_segment_init(void);
-void seven_segment_tick(void);
-void seven_segment_clear(void);
+void seq_seven_segment_init(void);
+void seq_seven_segment_tick(void);
+void seq_seven_segment_clear(void);
 
-void seven_segment_time(uint64_t us);
-void seven_segment_cycles(uint8_t cycles);
+void seq_seven_segment_time(uint64_t us);
+void seq_seven_segment_cycles(uint8_t cycles);
 
-void seven_segment_set(uint8_t data, uint8_t dot_pos);
+void seq_seven_segment_set(uint16_t data);
+void seq_seven_segment_set_prefix(uint8_t prefix, uint16_t data);
 
 #endif
