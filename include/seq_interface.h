6aa0ef021351b9debbabec6890957302675e66dc
DerBejijing <verunsicherungsgmbhundco@gmail.com>
2023-01-15 15:04:45 +0100

    add interface

--- include/seq_interface.h
+++ include/seq_interface.h
@@ -11,11 +11,11 @@ void seq_interface_tick(void);
 void seq_interface_change_write(void);
 void seq_interface_change_clear(void);
 
-void seq_mode_ratcheting(void);
-void seq_mode_duration(void);
-void seq_mode_gate(void);
+void seq_interface_mode_ratcheting(void);
+void seq_interface_mode_duration(void);
+void seq_interface_mode_gate(void);
 
-void seq_select_row_toggle_note(void);
-void seq_select_row_down(void);
+void seq_interface_select_row_toggle_note(void);
+void seq_interface_select_row_down(void);
 
 #endif
