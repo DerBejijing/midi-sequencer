6930c889233cef46672bea2e269a31f6ffe6b9f6
DerBejijing <verunsicherungsgmbhundco@gmail.com>
2023-01-24 19:29:20 +0100

    ratcheting!

--- src/seq_sequencer.c
+++ src/seq_sequencer.c
@@ -11,11 +11,11 @@ uint8_t seq_running = 0;
 uint64_t seq_join_last_clock = 0;                   // last clock for joined sequence
 uint64_t seq_join_last_ratchet = 0;                 // last ratchet for joined sequence
 
-uint64_t seq_us_per_beat = 500000;                 // speed of the sequence(s)
+uint64_t seq_us_per_beat = 500000;                  // speed of the sequence(s)
 
 uint8_t seq_values[SEQ_STAGES * SEQ_ROWS] = {0};
-uint8_t seq_durations[SEQ_STAGES * SEQ_ROWS] = {1};
-uint8_t seq_ratchets[SEQ_STAGES * SEQ_ROWS] = {1};
+uint8_t seq_durations[SEQ_STAGES * SEQ_ROWS] = {0}; // haha nice you cannot initialize it to 1 like this...
+uint8_t seq_ratchets[SEQ_STAGES * SEQ_ROWS] = {0};
 
 struct seq_row {
     uint8_t id;                 // aka row
@@ -58,6 +58,11 @@ void sequencer_init(void) {
         seq_rows[row].render = 1;
         seq_rows[row].last_clock = current_time;
         seq_rows[row].last_ratchet = current_time;
+    }
+
+    for(uint8_t i = 0; i < SEQ_ROWS * SEQ_STAGES; ++i) {
+        seq_ratchets[i] = 1;
+        seq_durations[i] = 1;
     }
 }
 
@@ -108,33 +113,6 @@ void sequencer_tick(void) {
             seq_gpio_matrix_set(play_row, play_stage);
         }
     } else {
-        /*
-        for(uint8_t row = 0; row < SEQ_ROWS; ++row) {
-            if(current_time >= seq_rows[row].last_clock + seq_us_per_beat) {
-                struct seq_row* current_row = &seq_rows[row];
-
-                current_row->last_clock = current_time;
-
-                if(current_row->active) {
-                    ++current_row->stage;
-
-                    if(current_row->stage >= current_row->stages) current_row->stage = 0;
-
-                    seq_gpio_matrix_set(row, current_row->stage);
-
-                    if(seq_terminate) if(current_row->stage == 0) {
-                        current_row->active = 0;
-                        seq_gpio_matrix_set(row, STAGE_NONE);
-                        return;
-                    }
-
-                    // play new note here
-                    printf("row [%d] stage [%d], start now: %d\n", row, current_row->stage, seq_values[row * SEQ_STAGES + current_row->stage]);
-                } else seq_gpio_matrix_set(row, STAGE_NONE);
-            }
-        }*/
-
-
         for(uint8_t row = 0; row < SEQ_ROWS; ++row) {
             struct seq_row* current_row = &seq_rows[row];
 
@@ -142,6 +120,7 @@ void sequencer_tick(void) {
 
             if(current_time >= current_row->last_clock + seq_us_per_beat) {
                 current_row->last_clock = current_time;
+                current_row->last_ratchet = current_time;
 
                 if(current_row->active) {
                     ++current_row->stage;
@@ -160,7 +139,7 @@ void sequencer_tick(void) {
             }
 
             uint8_t ratchets = seq_ratchets[SEQ_STAGES * row + current_row->stage];
-            uint64_t ratchet_time = seq_us_per_beat / 2;
+            uint64_t ratchet_time = seq_us_per_beat / ratchets;
 
             if(current_time >= current_row->last_ratchet + ratchet_time) {
                 current_row->last_ratchet = current_time;
@@ -171,7 +150,6 @@ void sequencer_tick(void) {
                 printf("row [%d] stage [%d], start now: %d\n", row, current_row->stage, seq_values[row * SEQ_STAGES + current_row->stage]);
             }
         }
-
     }
 
     if(seq_terminate) if(!seq_join) {
